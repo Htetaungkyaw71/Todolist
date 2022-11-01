de0a89a580dafc1d83036cfcecc371b290dbd84d
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-11-01 11:05:03 +0630

    Setup linters

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import './style.css';
 
 // function component() {
 //     const element = document.querySelector('h1');
-  
+
 //     element.classList.add('hello');
-  
+
 //     return element;
 //   }
-  
+
 //   document.body.appendChild(component());
 
-let div = document.querySelector('h1')
-div.innerHTML = 'Hello'
+const div = document.querySelector('h1');
+div.innerHTML = 'Hello';
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-mode:"development",
- entry: {
-   index: './src/index.js',
- },
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   devServer: {
     static: './dist',
   },
   output: {
-   filename: '[name].bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
