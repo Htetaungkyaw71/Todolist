b5df9a233d0b89476184ffa2e09886d0adc014a8
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-11-01 22:16:47 +0630

    Add edit function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 /* eslint-disable import/extensions */
 import './style.css';
-import {render,add,checkLocalStorage, tasks, ChangeEdit,  removeDom} from "./script.js";
+import {
+  render, add, checkLocalStorage, tasks, ChangeEdit, removeDom,
+} from './script.js';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
-
-
-
-document.getElementById('addForm').onsubmit = (e) =>{
-    e.preventDefault()
-    const value = e.target.task.value;
-    add({
-        id:Date.now().toString(),
-        description:value,
-        complete:false,
-        index:tasks.length
-    })
-    e.target.task.value = "";
-}
-
-
-
+document.getElementById('addForm').onsubmit = (e) => {
+  e.preventDefault();
+  const { value } = e.target.task;
+  add({
+    id: Date.now().toString(),
+    description: value,
+    complete: false,
+    index: tasks.length,
+  });
+  e.target.task.value = '';
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    checkLocalStorage()
-    tasks.sort((a, b) => a.index - b.index)
-        .map((item) => render(item));   
-    ChangeEdit()
-    removeDom()
+  checkLocalStorage();
+  tasks.sort((a, b) => a.index - b.index)
+    .map((item) => render(item));
+  ChangeEdit();
+  removeDom();
 });
--- src/script.js
+++ src/script.js
@@ -1,85 +1,122 @@
 const list = document.querySelector('.list-item');
 
+// eslint-disable-next-line import/no-mutable-exports
 let tasks = [];
 
-const add = (task) =>{
-    tasks.push(task)
-    updateLocalStorage()
-    render(task)
-    ChangeEdit()
-    removeDom()
-}
+const updateLocalStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-const remove = (id) =>{
-    let newtasks = []
-    tasks = tasks.filter(task => task.id !== id)
-    tasks.forEach((task,index)=>{
-        newtasks.push({...task,index})
-    })
-    tasks = newtasks;
-    updateLocalStorage()
-}
+const getTask = (id) => {
+  const editTask = tasks.filter((task) => task.id === id)[0];
+  return editTask;
+};
 
-const updateLocalStorage = ()=>{
-    localStorage.setItem("tasks",JSON.stringify(tasks)) 
-}
+const remove = (id) => {
+  const newtasks = [];
+  tasks = tasks.filter((task) => task.id !== id);
+  tasks.forEach((task, index) => {
+    newtasks.push({ ...task, index });
+  });
+  tasks = newtasks;
+  updateLocalStorage();
+};
 
-const checkLocalStorage = ()=>{
-    if(localStorage.getItem("tasks")){
-        tasks = JSON.parse(localStorage.getItem("tasks"))
-    } else {
-        localStorage.setItem("tasks",JSON.stringify([]))
-    }
-}
+const checkLocalStorage = () => {
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  } else {
+    localStorage.setItem('tasks', JSON.stringify([]));
+  }
+};
 
-const ChangeEdit = () =>{
-    document.querySelectorAll("#edit").forEach(btn=>{
-        btn.onclick = () =>{
-            const trash = btn.nextElementSibling;
-            const parentDiv = btn.parentElement.parentElement;
-            const input = btn.parentElement.previousElementSibling;
-            input.setAttribute('contenteditable', true)
-            parentDiv.style.background = "#DCDBA9";
-            trash.classList.remove('trash')
-            btn.classList.add('trash')    
-        }    
-    })
-}
-
-const removeDom = () =>{
-    document.querySelectorAll("#delete").forEach(btn=>{
-        btn.onclick = () =>{
-            const parentDiv = btn.parentElement.parentElement;
-            remove(parentDiv.id)
-            parentDiv.remove()
-        }    
-    })
-
-
-}
+const removeDom = () => {
+  document.querySelectorAll('#delete').forEach((btn) => {
+    btn.onclick = () => {
+      const parentDiv = btn.parentElement.parentElement;
+      remove(parentDiv.id);
+      parentDiv.remove();
+    };
+  });
+};
 
 const render = (task) => {
-    const div = document.createElement('div');
-    div.innerHTML = `
-      <h3>
-          <input type="checkbox" class="check">
-              ${task.description}
+  const div = document.createElement('div');
+  div.innerHTML = `
+      <div class="item">
+      <input type="checkbox" class="check">
+      <h3>        
+      ${task.description}
       </h3>
-          <div>
-            <button id="edit">
-                <i class="fa-solid fa-ellipsis-vertical"></i>
-            </button>
-            <button id="delete" class="trash">
-                <i class="fa-solid fa-trash-can"></i>
-            </button>
+      <form action="" id="editForm" class="trash">         
+          <input type="text" value="${task.description}" name="task" required>
+      </form>
+      </div>
+     
+        
+            <div>
+              <button id="edit">
+                  <i class="fa-solid fa-ellipsis-vertical"></i>
+              </button>
+              <button id="delete" class="trash">
+                  <i class="fa-solid fa-trash-can"></i>
+              </button>
+  
+               
+            </div>
+        `;
+  div.classList.add('list');
+  div.id = task.id;
+  list.appendChild(div);
+};
 
-             
-          </div>
-      `;
-    div.classList.add('list');
-    div.id = task.id
-    list.appendChild(div);
-  };
+const edit = (id, item) => {
+  const editTask = getTask(id);
+  tasks.splice(editTask.index, 1, item);
+  updateLocalStorage();
+  removeDom();
+};
 
+const ChangeEdit = () => {
+  document.querySelectorAll('#edit').forEach((btn) => {
+    btn.onclick = () => {
+      // button
+      const trash = btn.nextElementSibling;
+      const parentDiv = btn.parentElement.parentElement;
+      const form = btn.parentElement.previousElementSibling.lastElementChild;
+      const text = btn.parentElement.previousElementSibling.firstElementChild.nextElementSibling;
+      form.classList.remove('trash');
+      text.classList.add('trash');
+      parentDiv.style.background = '#DCDBA9';
+      trash.classList.remove('trash');
+      btn.classList.add('trash');
+      // button
 
-export {add,render, updateLocalStorage, checkLocalStorage, tasks, ChangeEdit, removeDom}
+      form.onsubmit = (e) => {
+        e.preventDefault();
+        const { value } = e.target.task;
+        const { id } = e.target.parentElement.parentElement;
+        const item = getTask(id);
+        edit(id, { ...item, description: value });
+        form.classList.add('trash');
+        text.classList.remove('trash');
+        text.innerHTML = value;
+        parentDiv.style.background = 'white';
+        trash.classList.add('trash');
+        btn.classList.remove('trash');
+      };
+    };
+  });
+};
+
+const add = (task) => {
+  tasks.push(task);
+  updateLocalStorage();
+  render(task);
+  ChangeEdit();
+  removeDom();
+};
+
+export {
+  add, render, updateLocalStorage, checkLocalStorage, tasks, ChangeEdit, removeDom, getTask,
+};
